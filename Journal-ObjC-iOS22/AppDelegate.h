38f5eb3dce8fb2405faec82176388e02f39e0bec
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-10-29 14:09:42 -0600

    Initial Commit

---
+++ Journal-ObjC-iOS22/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-ObjC-iOS22
+//
+//  Created by Ivan Ramirez on 10/29/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal-ObjC-iOS22/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal-ObjC-iOS22
+//
+//  Created by Ivan Ramirez on 10/29/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
